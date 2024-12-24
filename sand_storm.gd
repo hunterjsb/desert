8c6ea002231a16d0da2857ea8e871be17acf488d
hunterjsb <hunterjsb@gmail.com>
2024-12-24 17:23:00 -0500

    shield gen texture and darkening effect

--- sand_storm.gd
+++ sand_storm.gd
@@ -1,10 +1,14 @@
 extends Area3D
 
 @export var damage = 1              # The amount of damage each tick
-@export var damage_ticks = 5        # Number of damage "ticks" per second
+@export var damage_ticks = 3        # Number of damage "ticks" per second
+@export var storm_darkening = 0.8
+@export var storm_darkening_time = 1.0
 
 var players_in_storm: Array = []
 var time_accum = 0.0
+
+@onready var sun = $"../../DirectionalLight3D"
 
 func _ready() -> void:
 	# Use the correct syntax for connecting signals:
@@ -27,12 +31,14 @@ func _process(delta: float) -> void:
 
 func _on_sand_storm_body_entered(body: Node):
 	if body.name == "Player":
-		# The object is the player; store a reference
 		players_in_storm.append(body)
 		print("Player entered the Sand Storm")
+		var t = create_tween()
+		t.tween_property(sun, "light_energy", 1 - storm_darkening, storm_darkening_time)
 
 func _on_sand_storm_body_exited(body: Node):
 	if body.name == "Player":
-		# Remove the player from the list
 		players_in_storm.erase(body)
 		print("Player exited the Sand Storm")
+		var t = create_tween()
+		t.tween_property(sun, "light_energy", 1, storm_darkening_time)
