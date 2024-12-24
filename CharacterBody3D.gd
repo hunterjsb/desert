6f6c2614a6769ec450423c53c538debe947a38ed
hunterjsb <hunterjsb@gmail.com>
2024-12-24 00:13:12 -0500

    shield area and hp bar

--- CharacterBody3D.gd
+++ CharacterBody3D.gd
@@ -20,14 +20,18 @@ var walk_cycle_time = 0.0
 var original_camera_local_pos
 var is_sprinting = false
 
-# Reference to the shield scene
+# Shield
 var shield_scene = preload("res://shield.tscn")
-
-# Tracking the carried shield
 var has_shield = true
 var carried_shield
 
+@onready var ray = $Camera3D/RayCast3D
+
+
 func _ready():
+	var ui_scene = preload("res://hud.tscn").instantiate()
+	add_child(ui_scene)
+
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	original_camera_local_pos = $Camera3D.transform.origin
 	
@@ -74,28 +78,48 @@ func _input(event):
 		rotate_y(-event.relative.x * mouse_sensitivity / 1000)
 		$Camera3D.rotate_x(-event.relative.y * mouse_sensitivity / 1000)
 		$Camera3D.rotation.x = clampf($Camera3D.rotation.x, -deg_to_rad(70), deg_to_rad(70))
+		
+	# Mouse Release/Recapture
+	if event.is_action_pressed("esc"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED else Input.MOUSE_MODE_CAPTURED
 
-	# Throw shield if user presses the assigned key and we have one
 	if event.is_action_pressed("throw_shield") and has_shield and carried_shield:
 		has_shield = false
-
-		# Convert from static to dynamic mode if the shield is a RigidBody3D
 		carried_shield.freeze = false
 		carried_shield.freeze_mode = RigidBody3D.FREEZE_MODE_KINEMATIC
-
-		# Remove from player 'hand' and add to the scene
 		$Camera3D/HandPoint.remove_child(carried_shield)
 		get_parent().add_child(carried_shield)
-
-		# Match the shield's transform to the player's HandPoint
 		carried_shield.global_transform = $Camera3D/HandPoint.global_transform
-
-		# Apply an impulse forward so it "throws"
 		var throw_dir = -$Camera3D.transform.basis.z.normalized()
 		carried_shield.apply_central_impulse(throw_dir * 5.0)
-
-		# We no longer have a shield in hand
 		carried_shield = null
+	elif event.is_action_pressed("throw_shield") and not has_shield and not carried_shield:
+		var collider = get_ray_collider("interactable")
+		if collider:
+			pick_up_shield(collider)
+		
+	if event.is_action_pressed("interact") and not has_shield:
+		var collider = get_ray_collider("interactable")
+		if collider:
+			collider.toggle_bubble_shield()
+		
+func get_ray_collider(group: String):
+	if ray.is_colliding():
+		var collider = ray.get_collider()
+		if collider and collider.is_in_group(group):
+			return collider
+
+func pick_up_shield(shield):
+	shield.get_parent().remove_child(shield)
+	$Camera3D/HandPoint.add_child(shield)
+	shield.transform = Transform3D()
+	shield.collision_layer = 0
+	shield.collision_mask = 0
+	shield.freeze = true
+	shield.freeze_mode = RigidBody3D.FREEZE_MODE_STATIC
+	
+	carried_shield = shield
+	has_shield = true
 
 func _on_terrain_map_ready():
 	gravity = 9.8
@@ -103,13 +127,8 @@ func _on_terrain_map_ready():
 func _update_camera_sway():
 	var intensity = sprint_sway_intensity if is_sprinting else walk_sway_intensity
 	var frequency = sprint_sway_frequency if is_sprinting else walk_sway_frequency
-
-	# Vertical bob (up/down)
 	var vertical_offset = sin(walk_cycle_time * frequency) * intensity
-	
-	# Horizontal sway (slight side-to-side)
 	var horizontal_offset = sin(walk_cycle_time * frequency * 0.5) * intensity * 0.5
-	
 	$Camera3D.transform.origin = original_camera_local_pos + Vector3(horizontal_offset, vertical_offset, 0)
 
 func _reset_camera_sway():
--- shield.gd
+++ shield.gd
@@ -1,8 +1,45 @@
 extends RigidBody3D
 
-func _ready():
-	pass
+@export var tween_time = 0.6
 
+@onready var bubble_shield_area = $BubbleShield
+var is_active := false
+
+const MIN_SCALE = 0.001
+
+func _ready():
+	bubble_shield_area.visible = false
+	bubble_shield_area.monitoring = false
+	bubble_shield_area.monitorable = false
+	bubble_shield_area.scale = Vector3(MIN_SCALE, MIN_SCALE, MIN_SCALE)
+
+func toggle_bubble_shield():
+	is_active = !is_active
+	
+	if is_active:
+		bubble_shield_area.visible = true
+		bubble_shield_area.monitoring = true
+		bubble_shield_area.monitorable = true
+		bubble_shield_area.scale = Vector3(MIN_SCALE, MIN_SCALE, MIN_SCALE)
+
+		create_tween().tween_property(
+				bubble_shield_area, 
+				"scale", 
+				Vector3.ONE,
+				tween_time
+			).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+	else:
+		create_tween().tween_property(
+				bubble_shield_area,
+				"scale",
+				Vector3(MIN_SCALE, MIN_SCALE, MIN_SCALE),
+				tween_time
+			).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN)
+		
+		await get_tree().create_timer(tween_time).timeout
+		bubble_shield_area.visible = false
+		bubble_shield_area.monitoring = false
+		bubble_shield_area.monitorable = false
 
 func _process(delta):
 	if global_position.y < -100:
