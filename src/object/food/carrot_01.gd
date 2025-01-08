747153471c4366726abdf0ef609409ed1641ace8
hunter <hunterjsb@gmail.com>
2025-01-07 23:10:34 -0500

    eat carrot and black hunger text

--- src/object/food/carrot_01.gd
+++ src/object/food/carrot_01.gd
@@ -1,1 +1,11 @@
 extends InteractableBody3D
+
+@export var hunger_restore_amount = 15
+
+
+func interact(player: Node) -> void:
+	if not player:
+		return
+	if "eat_food" in player:
+		player.eat_food(hunger_restore_amount)
+	queue_free()
