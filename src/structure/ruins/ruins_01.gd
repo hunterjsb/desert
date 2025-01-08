af31c95f267d61eb9ba4bd89ae431adfc118c6f4
hunter <hunterjsb@gmail.com>
2025-01-07 23:38:34 -0500

    eat carrot and carrot spawn in world

--- src/structure/ruins/ruins_01.gd
+++ src/structure/ruins/ruins_01.gd
@@ -1,9 +1,8 @@
 extends Node3D
 
 var env: Node3D
-var radio: Node3D
+var spawned_loot: Node3D = null  # Track the currently spawned loot
 @onready var detection_area: Area3D = $DetectionArea
-
 
 func _set_transform(position: Vector3, rotation_y: float, scale_factor: float):
 	global_transform.origin = position
@@ -11,12 +10,28 @@ func _set_transform(position: Vector3, rotation_y: float, scale_factor: float):
 	scale *= Vector3(scale_factor, scale_factor, scale_factor)
 
 func spawn_loot(position: Vector3):
-	radio = preload("res://src/object/radio.tscn").instantiate()
-	radio.env = env
-	get_parent().add_child(radio)
-	radio.call_deferred("set_global_position", position)
-	radio.call_deferred("freeze")
+	var rand = randi() % 100  # Generate a random number between 0 and 99
+
+	if rand < 60:
+		# 60% chance: No loot
+		return
+	elif rand < 90:
+		# 30% chance: Spawn a carrot
+		spawned_loot = preload("res://src/object/food/carrot.tscn").instantiate()
+	else:
+		# 10% chance: Spawn a radio
+		spawned_loot = preload("res://src/object/radio.tscn").instantiate()
+		spawned_loot.env = env
+
+	# Add the loot to the parent and set position
+	get_parent().add_child(spawned_loot)
+	spawned_loot.call_deferred("set_global_position", position)
+
+	# Ensure loot is frozen initially
+	if "_freeze" in spawned_loot:
+		spawned_loot.call_deferred("_freeze")
 
 func _on_body_entered(body: Node3D) -> void:
-	if radio:
-		radio.unfreeze()
+	# Unfreeze the loot if it has the method
+	if spawned_loot and "_unfreeze" in spawned_loot:
+		spawned_loot._unfreeze()
