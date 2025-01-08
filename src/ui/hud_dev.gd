79a56017f12ae11f93c08005d63fd3480db8c913
hunter <hunterjsb@gmail.com>
2025-01-07 22:04:25 -0500

    fixed time

--- src/ui/hud_dev.gd
+++ src/ui/hud_dev.gd
@@ -7,16 +7,17 @@ extends Control
 @onready var coords = $CoordsLabel
 @onready var time = $TimeLabel
 @onready var wind = $WindLabel
+@onready var hunger = $HungerLabel
 
 
 func _process(delta: float) -> void:
-	coords.text = "%s" % snapped(player.global_position, Vector3(0.01, 0.01, 0.01))
-	
-	# var time_of_day_minutes = environment.time_of_day_minutes
-	# time.text = "%02d:%02d" % [time_of_day_minutes / 60, time_of_day_minutes % 60]
-	
-	var wind_angle = wind_man.major_dir_angle_current
-	wind.text = "Wind: %s" % deg_to_compass(wind_angle)
+	if visible:
+		coords.text = "%s" % snapped(player.global_position, Vector3(0.01, 0.01, 0.01))
+		
+		time.text = "%02dhr" % environment.day_time
+		
+		var wind_angle = wind_man.major_dir_angle_current
+		wind.text = "Wind: %s (%d)" % [deg_to_compass(wind_angle), wind_angle]
 	
 func deg_to_compass(degrees: float) -> String:
 	var compass = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]
