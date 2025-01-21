92774bf91bbe40aafb93a52bf512b67a1d08c952
hunter <hunterjsb@gmail.com>
2025-01-20 22:07:40 -0500

    sticky object

--- src/object/sticky/snappy_body.gd
+++ src/object/sticky/snappy_body.gd
@@ -8,6 +8,11 @@ func _ready() -> void:
 	pass
 
 func _on_snap_area_area_entered(area: Area3D) -> void:
+	print(area)
+	if not area.is_in_group("sticky"):
+		print("NOT STICKY")
+		return
+	
 	if carrying_player:
 		carrying_player.clear_hand()
 	call_deferred("reparent", area.get_parent())
